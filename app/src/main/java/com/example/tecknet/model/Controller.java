b04e77a21ce41c62f28fcddc7754ce1c06318b6c
mor234 <moriya264@gmail.com>
2021-11-25 10:17:06 +0200

    implement Contact, with constructors

---
+++ app/src/main/java/com/example/tecknet/model/Controller.java
@@ -0,0 +1,76 @@
+package com.example.tecknet.model;
+
+import java.util.ArrayList;
+
+public class Controller implements ControllerInt{
+//    /**
+//     * Get user from data base by email and password
+//     *
+//     * @param Email
+//     * @param password
+//     * @return
+//     */
+//    @Override
+//    public User get_user(String Email, String password) {
+//        return null;
+//    }
+//
+//    /**
+//     * Keep new user in data base
+//     *
+//     * @param user
+//     * @param password
+//     */
+//    @Override
+//    public void set_user(User user, String password) {
+//
+//    }
+//
+//    /**
+//     * Get malfunctions assigned to tech from database
+//     *
+//     * @param tech
+//     * @param amount - mount of MalfunctionDetails to get from data base
+//     * @return malfunctions tech opened is assigned to
+//     */
+//    @Override
+//    public ArrayList<MalfunctionDetails> get_tech_malfunction(TechnicianInt tech, int amount) {
+//        return null;
+//    }
+//
+//    /**
+//     * Get malfunctions mm opened from database
+//     *
+//     * @param mm     - maintenance man
+//     * @param amount - amount of MalfunctionDetails to get from data base
+//     * @param all    - if true bring every MalfunctionDetails mm opened, else bring only the one that wasn't closed
+//     * @return malfunctions mm opened
+//     */
+//    @Override
+//    public ArrayList<MalfunctionDetails> get_mm_malfunction(MaintenanceManInt mm, int amount, boolean all) {
+//        return null;
+//    }
+//
+//    /**
+//     * Get all un-assigned  malfunction from database
+//     *
+//     * @param tech
+//     * @param amount - amount of MalfunctionDetails to get from data base
+//     * @return
+//     */
+//    @Override
+//    public ArrayList<MalfunctionDetails> get_open_malfunction(TechnicianInt tech, int amount) {
+//        return null;
+//    }
+
+    /**
+     * Add  MalfuntionDetails to data base
+     *
+     * @param user
+     * @param mal
+     */
+    @Override
+    public void set_malfunction(User user, MalfunctionDetails mal) {
+
+    }
+}
