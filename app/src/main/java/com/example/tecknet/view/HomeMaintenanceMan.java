606a9438cc3a3d90500498b1d6f220e630a903e0
mor234 <moriya264@gmail.com>
2021-12-08 18:01:57 +0200

    added pages for side bar maintenance man. still need to be connected to the rest of the project

---
+++ app/src/main/java/com/example/tecknet/view/HomeMaintenanceMan.java
@@ -0,0 +1,67 @@
+package com.example.tecknet.view;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.tecknet.R;
+import com.example.tecknet.databinding.ActivityMainMainManBinding;
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeMaintenanceMan extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainMainManBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainMainManBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+//        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {//TODO EARASE
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(HomeMaintenanceMan.this, ReportMalfunctionFragment.class));
+//
+////                NavController navController = Navigation.findNavController(view);
+////                navController.navigate(R.id.action_nav_home_to_nav_report_malfunction2);
+//            }
+//        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_report_malfunction, R.id.nav_inventory)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_maintenance_man, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tecknet/view/inventory_maintenance_man/InventoryFragment.java
@@ -0,0 +1,45 @@
+package com.example.tecknet.view.inventory_maintenance_man;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.tecknet.databinding.FragmentInventoryMaintenanceManBinding;
+
+public class InventoryFragment extends Fragment {
+
+    private InventoryViewModel inventoryViewModel;
+    private FragmentInventoryMaintenanceManBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        inventoryViewModel =
+                new ViewModelProvider(this).get(InventoryViewModel.class);
+
+        binding = FragmentInventoryMaintenanceManBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSlideshow;
+        inventoryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
