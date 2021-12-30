265088d36a910b80c5bb6f2d92220539fc21cfb6
yuval yahod <yuvalpe000@gmail.com>
2021-12-30 16:53:33 +0200

    add connect_db to shared controller

--- app/src/main/java/com/example/tecknet/controller/shared_controller.java
+++ app/src/main/java/com/example/tecknet/controller/shared_controller.java
@@ -1,4 +1,14 @@
 package com.example.tecknet.controller;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public abstract class shared_controller {
+
+
+
+    protected static DatabaseReference connect_db(String db) {
+        FirebaseDatabase rootNode = FirebaseDatabase.getInstance(); //connect to firebase
+        return rootNode.getReference(db);
+    }
 }
