44359928ca79ff052d23817da4009b7e0464efc2
Nadavkeysar <nadavkasoker@gmail.com>
2022-01-03 19:31:31 +0200

    sms from tech

--- app/src/main/java/com/example/tecknet/view/my_malfunctions/MyMalfunctionsAdapter.java
+++ app/src/main/java/com/example/tecknet/view/my_malfunctions/MyMalfunctionsAdapter.java
@@ -6,6 +6,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.media.app.NotificationCompat;
+//import androidx.media.app.NotificationCompat;
 
 import com.example.tecknet.R;
 import com.example.tecknet.controller.technician_controller;
@@ -114,7 +115,9 @@ public class MyMalfunctionsAdapter extends ArrayAdapter<malfunctionView> {
                             String pay = edittext.getText().toString();
                             double payment = Double.parseDouble(pay);
                             technician_controller.set_payment_nalfunction(mal.getMal_id(), payment);
+                            sendSMS(payment);
                             technician_controller.set_status_nalfunction(mal.getMal_id(), "מחכה לתשלום");
+
                         }
                     });
 
@@ -133,17 +136,12 @@ public class MyMalfunctionsAdapter extends ArrayAdapter<malfunctionView> {
         return convertView;
     }
 
-    private void notification(double pay){
-//        String ticker = "הודעה חדשה";
-//        String title = "התקלה טופלה בהצלחה!";
-//        String text = "התקלה במכשיר" + product.getType() + " - " + mal.getExplanation()
-//                + "תוקנה בהצלחה. מחיר הטיפול הינו" + pay + "ש\"ח";
-//        Notification.Builder builder = new Notification.Builder(mContext)
-//                .setTicker(ticker)
-//                .setContentTitle(title)
-//                .setContentText(text)
-//                .setSmallIcon(R.drawable.technetlogo);
-//
-//        builder.build();
+    private void sendSMS(double pay){
+        String title = "התקלה טופלה בהצלחה!";
+        String text = "התקלה במכשיר" + product.getType() + " - " + mal.getExplanation()
+                + "תוקנה בהצלחה. מחיר הטיפול הינו" + pay + "ש\"ח";
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(ins.getPhone_number(), null, text, null, null);
+
     }
 }
